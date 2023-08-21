4f1724e22ad0ded854b3a5a9df54507d98744c64
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-08-21 19:11:26 +1030

    bool20.py

--- bool20.py
+++ bool20.py
@@ -6,5 +6,7 @@ def main(a):
     Returns:
         bool
     """
-    # Write your code here
-    return 
+    # Write your code here 9876
+    return a<10000 and ((a%10)+(a//10%10)+(a//100%10)+(a//1000))%2==1 and ((a%10)+(a//10%10)+(a//100%10)+(a//1000))%2!=0 and a>0 
+print(main(int(input())))
+    
